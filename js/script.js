9ca4f86e493f7060652e081b38a5efbfb1bd59cc
Milosz Bargiel <miloszbargiel@gmail.com>
2022-03-03 14:27:05 +0100

    Add functions for moves and results

--- js/script.js
+++ js/script.js
@@ -1,43 +1,43 @@
-let computerMove = "unknown";
-
 let moveRandomiser = Math.floor(Math.random() * 3 + 1);
+
+let computerMove = getMoveName(moveRandomiser);
 
 console.log("Generated number is " + moveRandomiser);
 
-if (moveRandomiser == 1) {
-  computerMove = "rock";
-} else if (moveRandomiser == 2) {
-  computerMove = "paper";
-} else {
-  computerMove = "scissors";
+function getMoveName(moveId) {
+  if (moveId == 1) {
+    return "rock";
+  } else if (moveId == 2) {
+    return "paper";
+  } else if (moveId == 3) {
+    return "scissors";
+  } else {
+    printMessage(moveId + " is an unknown moveID.");
+  }
 }
 
 let playerInput = prompt("Enter your move! 1: rock, 2: paper, 3: scissors.");
 
 console.log("Player entered: " + playerInput);
 
-let playerMove = "unknown";
+let playerMove = getMoveName(playerInput);
 
-if (playerInput == 1) {
-  playerMove = "rock";
-} else if (playerInput == 2) {
-  playerMove = "paper";
-} else if (playerInput == 3) {
-  playerMove = "scissors";
+function displayResult(computerMove, playerMove) {
+  printMessage(
+    "I played " + computerMove + " and you played " + playerMove + " ."
+  );
+
+  if (computerMove == playerMove) {
+    printMessage("It's a draw!");
+  } else if (
+    (computerMove == "rock" && playerMove == "paper") ||
+    (computerMove == "paper" && playerMove == "scissors") ||
+    (computerMove == "scissors" && playerMove == "rock")
+  ) {
+    printMessage("You win!");
+  } else {
+    printMessage("You lose!");
+  }
 }
 
-if (computerMove == playerMove) {
-  printMessage("It's a draw!");
-} else if (computerMove == "rock" && playerMove == "scissors") {
-  printMessage("You lose!");
-} else if (computerMove == "scissors" && playerMove == "paper") {
-  printMessage("You lose!");
-} else if (computerMove == "paper" && playerMove == "rock") {
-  printMessage("You lose!");
-} else if (computerMove == "scissors" && playerMove == "rock") {
-  printMessage("You win!");
-} else if (computerMove == "paper" && playerMove == "scissors") {
-  printMessage("You win!");
-} else {
-  printMessage("This is not a valid move!");
-}
+displayResult(computerMove, playerMove);
